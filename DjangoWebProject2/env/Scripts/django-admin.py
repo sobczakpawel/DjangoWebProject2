c45cbf79ec69eb575115e7b51f32c7ff36ccd257
Pawel <Pawel@DESKTOP-NPUB99M>
2017-07-26 08:03:50 +0200

    1

---
+++ DjangoWebProject2/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pawel\documents\visual studio 2017\Projects\DjangoWebProject2\DjangoWebProject2\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
